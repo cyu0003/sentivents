6ed85bdd6ddfe40417e50a2b0f3581f5f2eecdd8
Kyle Zheng <kylezheng73@gmail.com>
2021-01-23 05:52:22 -0500

    calendar tab start

--- src/Screens/Graph.js
+++ src/Screens/Graph.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
-import { View, Text, Button, ScrollView } from 'react-native';
-import {MonthCalendarGraph, RecentDaysCalendarGraph, YearCalendarGraph} from '../components/CalendarGraph'
-
+import React, { Component } from "react";
+import { View, Text, Button, ScrollView } from "react-native";
+import {
+  MonthCalendarGraph,
+  RecentDaysCalendarGraph,
+  YearCalendarGraph,
+} from "../components/CalendarGraph";
+import { LineGraph } from "../components/LineGraph";
+import { Picker } from "@react-native-picker/picker";
 export default class Graph extends Component {
-    render () {
-        return (
-            <ScrollView>
-                <Text>This is where a graph goes</Text>
-                <MonthCalendarGraph year={2021} month={0}/>
-                <RecentDaysCalendarGraph days={69}/>
-                <YearCalendarGraph year={2021}/>
-            </ScrollView>
-        );
-    }
+  render() {
+    return (
+      <ScrollView>
+        <Picker
+          style={{height: 50, width: 100}}
+          mode={"dropdown"}>
+          <Picker.Item label="Java" value="java" />
+          <Picker.Item label="JavaScript" value="js" />
+        </Picker>
+        <Text>This is where a graph goes</Text>
+        <LineGraph />
+        <MonthCalendarGraph year={2021} month={0} />
+        <RecentDaysCalendarGraph days={69} />
+        <YearCalendarGraph year={2021} />
+      </ScrollView>
+    );
+  }
 }
