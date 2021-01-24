1dfd642a3004ce08f50beac2a333aee50a7334f9
Christopher Yu <chris.b.yu@gmail.com>
2021-01-24 03:36:29 -0500

    fixed onPress2 in HomeScreen

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -25,6 +25,7 @@ export default class HomeScreen extends Component {
         };
 
         this.onPress = this.onPress.bind(this);
+        this.onPress2 = this.onPress2.bind(this);
     }
 
     componentDidMount() {
